475d84fb980c239e4b308f1de1583d8b3462a446
Fredrik B <fredrik.bogren@hotmail.com>
2019-06-19 19:58:02 +0200

    first commit

---
+++ IU1/Controllers/CoordinatorController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace IU1.Controllers
+{
+    public class CoordinatorController : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult CrimeCoordinator()
+        {
+            return View();
+        }
+      public IActionResult ReportCrime()
+      {
+        return View();
+      }
+      public IActionResult StartCoordinator()
+      {
+        return View();
+      }
+      public IActionResult Thanks()
+      {
+        return View();
+      }
+      public IActionResult Validate()
+      {
+        return View();
+      }
+  }
+}
---
+++ IU1/Controllers/InvestigatorController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace IU1.Controllers
+{
+    public class InvestigatorController : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult CrimeInvestigator()
+        {
+            return View();
+        }
+      public IActionResult StartInvestigator()
+      {
+        return View();
+      }
+  }
+}
